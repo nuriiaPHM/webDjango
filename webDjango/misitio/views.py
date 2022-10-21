690bffd8a0c3545a068351105283195f2b828653
Nuria <nuriajalda@gmail.com>
2022-10-21 13:33:18 +0200

    221021

--- webDjango/misitio/views.py
+++ webDjango/misitio/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-
+from .models import Cliente
+from django.utils import timezone
 # Create your views here.
 
 def clientes_list(request):
-    return render(request, 'misitio/clientes_list.html', {})
+    clientes = Cliente.objects.filter(alta__lte=timezone.now()).order_by('alta')
+    return render(request, 'misitio/clientes_list.html', {'clientes':clientes})
